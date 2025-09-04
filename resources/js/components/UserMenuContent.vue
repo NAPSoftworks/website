2c4f892b5b36d29f495299a9f73a6dc8ac956259
Talky <talky@napsoftworks.dev>
2025-09-04 22:51:09 +0200

    Add Dashboard button to logged user's menu

--- resources/js/components/UserMenuContent.vue
+++ resources/js/components/UserMenuContent.vue
@@ -25,6 +25,12 @@ defineProps<Props>();
     <DropdownMenuSeparator />
     <DropdownMenuGroup>
         <DropdownMenuItem :as-child="true">
+            <Link class="block w-full" :href="route('dashboard')" prefetch as="button">
+                <Settings class="mr-2 h-4 w-4" />
+                Dashboard
+            </Link>
+        </DropdownMenuItem>
+        <DropdownMenuItem :as-child="true">
             <Link class="block w-full" :href="route('profile.edit')" prefetch as="button">
                 <Settings class="mr-2 h-4 w-4" />
                 Settings
